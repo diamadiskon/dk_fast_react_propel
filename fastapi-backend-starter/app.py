826d410adb888e10c9ecd2036fdb32ce29446f9f
Diamadis <diamadisapple@gmail.coms>
2024-02-04 16:48:32 +0200

    trying to depl the app aks

--- fastapi-backend-starter/app.py
+++ fastapi-backend-starter/app.py
@@ -12,7 +12,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "localhost:3000",
-    "http://health-data.westeurope.cloudapp.azure.com/"
+    "http://health-data.westeurope.cloudapp.azure.com/api"
 ]
 
 
