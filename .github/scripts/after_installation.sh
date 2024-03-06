97dbede916148cafe95bff85dd99ab7ed0febea9
Diamadis <diamadisapple@gmail.coms>
2024-03-06 13:40:25 +0200

    scripot for webapp

--- .github/scripts/after_installation.sh
+++ .github/scripts/after_installation.sh
@@ -4,7 +4,7 @@ sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker
 az acr login --name acrfastreactdevneuotldn
 git clone https://github.com/diamadiskon/dk_fast_react_propel.git
 cd dk_fast_react_propel || exit
-sudo chmod +x docker-compose
+sudo chmod +x docker-compose.yml
 sudo docker-compose build
 sudo docker tag fullstack-react-fastapi-propel-backend acrfastreactdevneuotldn.azurecr.io/app/backend
 sudo docker tag fullstack-react-fastapi-propel-frontend acrfastreactdevneuotldn.azurecr.io/app/frontend
