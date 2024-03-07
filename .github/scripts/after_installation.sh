00ca70a56be291f5e8a41a3d4b89ed6d6fdf1593
Diamadis <diamadisapple@gmail.coms>
2024-03-07 12:10:49 +0200

    cors

--- .github/scripts/after_installation.sh
+++ .github/scripts/after_installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 az login --identity
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-az acr login --name acrfastreactdevneuotldn
+sudo az acr login --name acrfastreactdevneuotldn
 git clone https://github.com/diamadiskon/dk_fast_react_propel.git
 cd dk_fast_react_propel || exit
 sudo chmod +x docker-compose.yml
